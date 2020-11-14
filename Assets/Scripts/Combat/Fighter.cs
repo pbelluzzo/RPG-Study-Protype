5c31c4074bcf8d2f2fe30a30ce8c041581106ed0
Pedro Belluzzo <pedrobelluzzo@gmail.com>
2020-11-14 19:57:19 -0300

    Basic attack done

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Core;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
-        [SerializeField] float weaponRange;
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float weaponDamage = 5f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
+        float timeSinceLastAttack = 9;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target != null && !GetIsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.position);
             }
             if (target != null && GetIsInRange())
             {
-                GetComponent<Mover>().Stop();
+                GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
         }
 
@@ -29,12 +36,27 @@ namespace RPG.Combat
 
         public void Attack(CombatTarget combatTarget)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.transform;
+            // Animation will trigger the Hit() event.
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack < timeBetweenAttacks) return;
+            timeSinceLastAttack = 0f;
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation Event
+        void Hit()
+        {
+            target.GetComponent<Health>().TakeDamage(weaponDamage);
         }
     }
 }
---
+++ Assets/Scripts/Core/ActionScheduler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class ActionScheduler : MonoBehaviour
+    {
+        IAction currentAction;
+
+        public void StartAction(IAction action)
+        {
+            print("current action is " + currentAction);
+            if (action == currentAction) return;    
+            if (currentAction != null)
+            {
+                currentAction.Cancel();
+                print("Starting " + action);
+            }
+            currentAction = action;
+        }
+
+    }
+
+}
